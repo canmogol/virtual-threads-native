94371db97982174cf22e38e108b46deb2b3eb7e5
Ali Can Mogol <ali.can.mogol@sentia.com>
2022-12-20 23:04:45 +0100

    Add JSON and Redis dependencies.

--- src/main/java/VirtualThreadApplication.java
+++ src/main/java/VirtualThreadApplication.java
@@ -1,6 +1,9 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -9,24 +12,49 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 public class VirtualThreadApplication implements HttpHandler {
-    final HttpClient httpClient = HttpClient
+    private final HttpClient httpClient = HttpClient
             .newBuilder()
             .executor(Executors.newVirtualThreadPerTaskExecutor())
             .build();
 
     public static void main(String[] args) throws IOException {
-        new VirtualThreadApplication().start();
+        final VirtualThreadApplication application = new VirtualThreadApplication();
+        application.start();
+    }
+
+    private void json() {
+        final String json = """
+                ["a", "b", "c"]
+                """;
+        List<String> abc = new Gson().fromJson(json, new TypeToken<List<String>>() {
+        }.getType());
+        System.out.println("abc = " + abc);
+    }
+
+    private void jedis() {
+        try {
+            Jedis jedis = new Jedis("localhost", 6379, 1);
+            jedis.set("key1", "value1");
+            String value1 = jedis.get("key1");
+            log("Cache value: %s".formatted(value1));
+        } catch (Exception e) {
+            log("Error on cache operation, error: %s, message: %s".formatted(e.getClass().getSimpleName(), e.getMessage()));
+        }
     }
 
     private void start() throws IOException {
-        final InetSocketAddress serverAddress = new InetSocketAddress("0.0.0.0", 7070);
+        json();
+        jedis();
+        final InetSocketAddress serverAddress = new InetSocketAddress("0.0.0.0", 8080);
         final HttpServer localhost = HttpServer.create(serverAddress, 100);
         localhost.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
         localhost.createContext("/", this);
         localhost.start();
+
         log("Server started.");
     }
 
@@ -36,15 +64,14 @@ public class VirtualThreadApplication implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exchange) {
-        log("Request: %s".formatted(exchange.getRequestURI()));
         handleRequest(exchange);
     }
 
     private void handleRequest(final HttpExchange exchange) {
         try {
-            HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(URI.create("http://192.168.68.118:9090/test.txt")).build();
-            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            String responseMessage = "Response: %s, isVirtual: %s".formatted(httpResponse.body().replace("\n", ""), Thread.currentThread().isVirtual());
+            final HttpRequest httpRequest = HttpRequest.newBuilder().GET().uri(URI.create("http://192.168.68.118:9090/test.txt")).build();
+            final HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            final String responseMessage = httpResponse.body().replace("\n", "");
             exchange.sendResponseHeaders(200, responseMessage.length());
             exchange.getResponseBody().write(responseMessage.getBytes(StandardCharsets.UTF_8));
             exchange.getResponseBody().close();
